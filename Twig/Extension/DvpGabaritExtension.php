f7f59b926db5fc96dc75b3b8e2c60aaf64bf935a
Thibaut Cuvelier <dourouc05@gmail.com>
2012-09-12 11:04:19 +0200

    New filter for right column with encoding.

--- Twig/Extension/DvpGabaritExtension.php
+++ Twig/Extension/DvpGabaritExtension.php
@@ -31,9 +31,10 @@ class DvpGabaritExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            'gab_up'      => new \Twig_Filter_Method($this, 'gabUp',      array('is_safe' => array('html'))),
-            'gab_down'    => new \Twig_Filter_Method($this, 'gabDown',    array('is_safe' => array('html'))),
-            'gab_license' => new \Twig_Filter_Method($this, 'gabLicense', array('is_safe' => array('html'))),
+            'gab_up'           => new \Twig_Filter_Method($this, 'gabUp',          array('is_safe' => array('html'))),
+            'gab_down'         => new \Twig_Filter_Method($this, 'gabDown',        array('is_safe' => array('html'))),
+            'gab_license'      => new \Twig_Filter_Method($this, 'gabLicense',     array('is_safe' => array('html'))),
+            'gab_right_column' => new \Twig_Filter_Method($this, 'gabRightColumn', array('is_safe' => array('html'))),
         );
     }
     
@@ -65,5 +66,9 @@ class DvpGabaritExtension extends \Twig_Extension
         return utf8_encode(str_replace('<?php
 echo $Annee;
 ?>', date('Y'), file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/template/licence3.php')));
+    }
+    
+    public function gabRightColumn($what) {
+        return utf8_encode(file_get_contents($what));
     }
 }
